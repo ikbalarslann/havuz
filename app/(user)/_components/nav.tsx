03d24f923e2528d7ceaae4a2dd9b5b37c453eeda
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-02 14:23:32 +0300

    nav functionality

--- app/(user)/_components/nav.tsx
+++ app/(user)/_components/nav.tsx
@@ -1,49 +1,107 @@
 "use client";
 
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@/components/auth/user-button";
-import { LoginButton } from "@/components/auth/login-button";
-import { RegisterButton } from "@/components/auth/register-button";
 import { useCurrentRole } from "@/hooks/use-current-role";
-import { UserRole } from "@prisma/client";
-import { LoginButtonDropDown } from "@/components/user/login-button";
+import { useState } from "react";
 
 export const Nav = () => {
-  const pathname = usePathname();
   const role = useCurrentRole();
 
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="bg-secondary flex justify-between items-center p-4  w-full shadow-sm">
-      <div className="flex gap-x-2">
-        <Button asChild variant={pathname === "/" ? "default" : "outline"}>
-          <Link href="/">Home</Link>
-        </Button>
-        <Button
-          asChild
-          variant={pathname === "/shoppingCard" ? "default" : "outline"}
-        >
+    <nav className="relative container mx-auto p-6">
+      <div className="flex items-center justify-between">
+        <div className="pt-2 text-5xl">
+          <Link href="/">H</Link>
+        </div>
+        <div className="hidden space-x-6 md:flex">
           <Link href="/shoppingCard">Shopping Card</Link>
-        </Button>
 
-        <Button
-          asChild
-          variant={pathname === "/bookings" ? "default" : "outline"}
-        >
-          <Link href="/bookings">Bookings</Link>
-        </Button>
+          {role && <Link href="/bookings">Bookings</Link>}
 
-        <Button
-          asChild
-          variant={pathname === "/settings" ? "default" : "outline"}
-        >
-          <Link href="/settings">Settings</Link>
-        </Button>
-        {!role && <LoginButtonDropDown />}
+          <div className="pl-32">
+            <UserButton />
+          </div>
+        </div>
+
+        {/* hamburger icon */}
+        <div className="md:hidden">
+          <button
+            id="menu-btn"
+            className={` block hamburger  focus:outline-none ${
+              isMenuOpen ? "open" : ""
+            }`}
+            onClick={toggleMenu}
+          >
+            <span className="hamburger-top"></span>
+            <span className="hamburger-middle"></span>
+            <span className="hamburger-bottom"></span>
+          </button>
+        </div>
       </div>
-      <UserButton />
+
+      <div className={`md:hidden ${isMenuOpen ? "block " : "hidden"}`}>
+        <div
+          id="menu"
+          className={`absolute flex-col items-center  self-end py-8 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md ${
+            isMenuOpen ? "flex" : "hidden"
+          }`}
+        >
+          <Link onClick={toggleMenu} href="/shoppingCard">
+            Shopping Card
+          </Link>
+
+          {role && (
+            <Link onClick={toggleMenu} href="/bookings">
+              Bookings
+            </Link>
+          )}
+
+          <UserButton toggleMenu={toggleMenu} />
+        </div>
+      </div>
     </nav>
+
+    // <nav className="bg-secondary flex justify-between items-center p-4  w-full shadow-sm">
+    //   <div className="flex gap-x-2">
+    //     <Button asChild variant={pathname === "/" ? "default" : "outline"}>
+    //       <Link href="/">Home</Link>
+    //     </Button>
+    //     <Button
+    //       asChild
+    //       variant={pathname === "/shoppingCard" ? "default" : "outline"}
+    //     >
+    //       <Link href="/shoppingCard">Shopping Card</Link>
+    //     </Button>
+
+    //     {role && (
+    //       <>
+    //         <Button
+    //           asChild
+    //           variant={pathname === "/bookings" ? "default" : "outline"}
+    //         >
+    //           <Link href="/bookings">Bookings</Link>
+    //         </Button>
+
+    //         <Button
+    //           asChild
+    //           variant={pathname === "/settings" ? "default" : "outline"}
+    //         >
+    //           <Link href="/settings">Settings</Link>
+    //         </Button>
+    //       </>
+    //     )}
+    //   </div>
+    //   <div>
+    //     {!role && <LoginButtonDropDown />}
+    //     <UserButton />
+    //   </div>
+    // </nav>
   );
 };
--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -1,42 +1,80 @@
 "use client";
 
-import { FaUser } from "react-icons/fa";
-import { ExitIcon } from "@radix-ui/react-icons"
+import { ExitIcon } from "@radix-ui/react-icons";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
+  DropdownMenuGroup,
 } from "@/components/ui/dropdown-menu";
-import {
-  Avatar,
-  AvatarImage,
-  AvatarFallback,
-} from "@/components/ui/avatar";
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { LogoutButton } from "@/components/auth/logout-button";
+import Link from "next/link";
+import { UserRole } from "@prisma/client";
+import { LoginButton } from "@/components/auth/login-button";
+import { RegisterButton } from "@/components/auth/register-button";
+import { useEffect, useState } from "react";
 
-export const UserButton = () => {
+export const UserButton = ({ toggleMenu }: any) => {
   const user = useCurrentUser();
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
 
-  return (
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsDesktop = window.innerWidth > 768;
+      setIsDesktop(newIsDesktop);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Call the handleResize function on initial render
+    handleResize();
+
+    // Remove the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const onClick = () => {
+    if (!isDesktop) {
+      toggleMenu();
+    }
+  };
+
+  return user ? (
     <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <AvatarImage src={user?.image || ""} />
-          <AvatarFallback className="bg-sky-500">
-            <FaUser className="text-white" />
-          </AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
+      <DropdownMenuTrigger>{user?.name}</DropdownMenuTrigger>
       <DropdownMenuContent className="w-40" align="end">
+        <Link href="/settings" onClick={onClick}>
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+        </Link>
         <LogoutButton>
           <DropdownMenuItem>
             <ExitIcon className="h-4 w-4 mr-2" />
             Logout
           </DropdownMenuItem>
         </LogoutButton>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  ) : (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Login</DropdownMenuTrigger>
+      <DropdownMenuContent className="w-20">
+        <DropdownMenuGroup>
+          <LoginButton asChild>
+            <DropdownMenuItem onClick={onClick}>Login</DropdownMenuItem>
+          </LoginButton>
+          <RegisterButton role={UserRole.USER} asChild>
+            <DropdownMenuItem onClick={onClick}>Register</DropdownMenuItem>
+          </RegisterButton>
+          <RegisterButton role={UserRole.ADMIN} asChild>
+            <DropdownMenuItem onClick={onClick}>Host Register</DropdownMenuItem>
+          </RegisterButton>
+        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   );
