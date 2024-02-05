df455b56ec0cb2b03f47e6de387e7394a1457480
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-05 05:22:38 +0300

    production bug test

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -5,24 +5,14 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 interface BackButtonProps {
-  href: string;
+  href?: string;
   label: string;
-};
+}
 
-export const BackButton = ({
-  href,
-  label,
-}: BackButtonProps) => {
+export const BackButton = ({ href, label }: BackButtonProps) => {
   return (
-    <Button
-      variant="link"
-      className="font-normal w-full"
-      size="sm"
-      asChild
-    >
-      <Link href={href}>
-        {label}
-      </Link>
+    <Button variant="link" className="font-normal w-full" size="sm" asChild>
+      {href && <Link href={href}>{label}</Link>}
     </Button>
   );
 };
