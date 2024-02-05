3488fa4bc97a0fc4f7ace64b3c00b0e88070ac0b
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-05 05:04:17 +0300

    production bug test

--- components/auth/card-wrapper.jsx
+++ components/auth/card-wrapper.jsx
@@ -10,21 +10,13 @@ import { Header } from "@/components/auth/header";
 import { Social } from "@/components/auth/social";
 import { BackButton } from "@/components/auth/back-button";
 
-interface CardWrapperProps {
-  children: React.ReactNode;
-  headerLabel: string;
-  backButtonLabel?: string;
-  backButtonHref?: string;
-  showSocial?: boolean;
-}
-
 export const CardWrapper = ({
   children,
   headerLabel,
   backButtonLabel,
   backButtonHref,
   showSocial,
-}: CardWrapperProps) => {
+}) => {
   return (
     <Card className="w-auto  shadow-md mb-10 px-6 pb-5 min-w-[350px]">
       <CardHeader>
