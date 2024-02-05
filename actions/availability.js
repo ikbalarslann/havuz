5506bd0e3fcc0370b9dec4076fdf3fdfeeb99f3f
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-05 04:11:03 +0300

    production bug test

--- actions/availability.js
+++ actions/availability.js
@@ -3,23 +3,11 @@
 import { db } from "@/lib/db";
 import { getPropertyByTitle } from "@/data/property";
 
-interface Availability {
-  date: string;
-  price: number;
-  free: boolean;
-}
-
-export const UpdatePropertyAvailability = async ({
-  title,
-  values,
-}: {
-  title: string;
-  values: Availability;
-}) => {
+export const UpdatePropertyAvailability = async ({ title, values }) => {
   const { date, price, free } = values;
   const property = await getPropertyByTitle(title);
   const availability = (property?.availability || []).filter(
-    (a): a is Availability => a?.date !== date
+    (a) => a?.date !== date
   );
   const updatedAvailability = [...availability, { date, price, free }];
 
