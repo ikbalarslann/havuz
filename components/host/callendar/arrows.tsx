4342aa6002f3b990784ec55471e396d039a7ee6f
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-09 12:48:25 +0300

    build warning to useMemo

--- components/host/callendar/arrows.tsx
+++ components/host/callendar/arrows.tsx
@@ -2,25 +2,28 @@
 
 import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
+
 const Arrows = ({ setMonth }: any) => {
   const [currentMonth, setCurrentMonth] = useState("01");
-  const arrayIndex = parseInt(currentMonth) - 1;
 
-  const month = [
-    { value: "01", label: "January" },
-    { value: "02", label: "February" },
-    { value: "03", label: "March" },
-    { value: "04", label: "April" },
-    { value: "05", label: "May" },
-    { value: "06", label: "June" },
-    { value: "07", label: "July" },
-    { value: "08", label: "August" },
-    { value: "09", label: "September" },
-    { value: "10", label: "October" },
-    { value: "11", label: "November" },
-    { value: "12", label: "December" },
-  ];
+  const month = useMemo(
+    () => [
+      { value: "01", label: "January" },
+      { value: "02", label: "February" },
+      { value: "03", label: "March" },
+      { value: "04", label: "April" },
+      { value: "05", label: "May" },
+      { value: "06", label: "June" },
+      { value: "07", label: "July" },
+      { value: "08", label: "August" },
+      { value: "09", label: "September" },
+      { value: "10", label: "October" },
+      { value: "11", label: "November" },
+      { value: "12", label: "December" },
+    ],
+    []
+  );
 
   useEffect(() => {
     const date = new Date();
@@ -31,30 +34,30 @@ const Arrows = ({ setMonth }: any) => {
   }, []);
 
   useEffect(() => {
-    setMonth(month[arrayIndex].value);
-  }, [currentMonth, arrayIndex, setMonth, month]);
+    setMonth(month[parseInt(currentMonth) - 1].value);
+  }, [currentMonth, setMonth, month]);
 
-  const increseMonth = () => {
+  const increaseMonth = () => {
     if (currentMonth === "12") return;
-    const numbermonth = parseInt(currentMonth);
-    const newMonth = numbermonth + 1;
+    const numberMonth = parseInt(currentMonth);
+    const newMonth = numberMonth + 1;
     const newMonthString = newMonth.toString();
-    currentMonth && setCurrentMonth(newMonthString);
+    setCurrentMonth(newMonthString);
   };
 
-  const decreseMonth = () => {
-    const numbermonth = parseInt(currentMonth);
-    if (numbermonth === 1) return;
-    const newMonth = numbermonth - 1;
+  const decreaseMonth = () => {
+    const numberMonth = parseInt(currentMonth);
+    if (numberMonth === 1) return;
+    const newMonth = numberMonth - 1;
     const newMonthString = newMonth.toString();
-    currentMonth && setCurrentMonth(newMonthString);
+    setCurrentMonth(newMonthString);
   };
 
   return (
-    <div className="flex justify-between w-[500px] ">
-      <ArrowLeftIcon className="w-6 h-6" onClick={decreseMonth} />
-      {month[arrayIndex].label}
-      <ArrowRightIcon className="w-6 h-6" onClick={increseMonth} />
+    <div className="flex justify-between w-[500px]">
+      <ArrowLeftIcon className="w-6 h-6" onClick={decreaseMonth} />
+      {month[parseInt(currentMonth) - 1].label}
+      <ArrowRightIcon className="w-6 h-6" onClick={increaseMonth} />
     </div>
   );
 };
