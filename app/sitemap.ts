5ccddd1ad0c46a9e787df7c642c10172bbb1944e
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-13 00:31:36 +0300

    sitemap bug fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,40 +15,40 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/error`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/error`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/login`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/login`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-password`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/new-password`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/new-verification`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/new-verification`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/register`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/register`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/auth/reset`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}auth/reset`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/bookings`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}bookings`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/discover`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}discover`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/pay`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}pay`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/settings`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}settings`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_APP_URL}/shoppingCard`,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}shoppingCard`,
     },
 
     ...postEntries,
