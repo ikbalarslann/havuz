b6fb41024c3a4edc7d839bd7fb46b45e962fe025
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-14 02:49:56 +0300

    testing single property page structured data

--- app/(user)/discover/[title]/page.tsx
+++ app/(user)/discover/[title]/page.tsx
@@ -1,6 +1,7 @@
 import SingleProperty from "@/components/user/single-property";
 import { Metadata } from "next";
 import { getPropertyByTitle } from "@/data/property";
+import StructuredData from "@/components/user/structured-data";
 
 export async function generateMetadata({
   params,
@@ -22,9 +23,67 @@ const SinglePropertyPage = async ({
 }) => {
   const title = params.title.split("-").join(" ");
   const property = await getPropertyByTitle(title);
+  const sobj = JSON.stringify(
+    property?.availability.find(
+      (a: any) => a?.date === new Date().toLocaleDateString("en-GB")
+    )
+  );
+  const obj = JSON.parse(sobj);
+  const price = obj?.price;
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Product",
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: "3.5",
+      reviewCount: "11",
+    },
+    description: property?.description,
+    name: property?.title,
+    image: property?.imgUrls[0],
+    offers: {
+      "@type": "Offer",
+      availability: "https://schema.org/InStock",
+      price: price,
+      priceCurrency: "TL",
+    },
+    review: [
+      {
+        "@type": "Review",
+        author: "Ellie",
+        datePublished: "2011-04-01",
+        reviewBody: "The lamp burned out and now I have to replace it.",
+        name: "Not a happy camper",
+        reviewRating: {
+          "@type": "Rating",
+          bestRating: "5",
+          ratingValue: "1",
+          worstRating: "1",
+        },
+      },
+      {
+        "@type": "Review",
+        author: "Lucas",
+        datePublished: "2011-03-25",
+        reviewBody:
+          "Great microwave for the price. It is small and fits in my apartment.",
+        name: "Value purchase",
+        reviewRating: {
+          "@type": "Rating",
+          bestRating: "5",
+          ratingValue: "4",
+          worstRating: "1",
+        },
+      },
+    ],
+  };
 
   return property ? (
-    <SingleProperty propertyObj={property} />
+    <>
+      <StructuredData data={jsonLd} />
+      <SingleProperty propertyObj={property} />
+    </>
   ) : (
     <div>
       <h1>Property not found</h1>
--- app/(user)/discover/page.tsx
+++ app/(user)/discover/page.tsx
@@ -1,30 +1,6 @@
 import { getAllProperties } from "@/data/property";
 import PropertyCard from "@/components/user/property-card";
-import StructuredData from "@/components/user/structured-data";
 import { Metadata } from "next";
-
-const jsonLd = {
-  "@context": "https://schema.org",
-  "@type": "BreadcrumbList",
-  itemListElement: [
-    {
-      "@type": "ListItem",
-      position: 1,
-      item: {
-        "@id": "https://havuzvehavuz.com/",
-        name: "Havuz ve Havuz",
-      },
-    },
-    {
-      "@type": "ListItem",
-      position: 2,
-      item: {
-        "@id": "https://havuzvehavuz.com/discover",
-        name: "Discover",
-      },
-    },
-  ],
-};
 
 export const metadata: Metadata = {
   title: "Discover",
@@ -36,7 +12,6 @@ const DiscoverPage = async () => {
 
   return (
     <div className="flex flex-col sm:flex-row gap-4  justify-center">
-      <StructuredData data={jsonLd} />
       {properties?.map((property) => (
         <div key={property.id}>
           <PropertyCard property={property} />
