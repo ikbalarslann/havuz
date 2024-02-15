1f74a72abc01d2cbc8dd51fe70b7fd50c61f997b
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-02-15 23:57:36 +0300

    deploy the ui improvements

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 const HomePage = async () => {
   return (
     <div className="flex flex-col sm:flex-row gap-4  justify-center">
-      <h1 className="text-center p-1 text-xl font-bold   text-blue-100  mb-10">
+      <h1 className="text-center p-1 text-xl font-bold   text-cyan-700  mb-10">
         Daily Pool Rentals in Istanbul
       </h1>
 
